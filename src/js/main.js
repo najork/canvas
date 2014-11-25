c5d1b87e80cd3002d11f748483d16cb82a354039
Zachary Nowicki <nowickiz@umich.edu>
2014-11-24 23:27:38 -0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -10,16 +10,20 @@
  * @author Dustin Bui, Maximilian Najork, Zachary Nowicki, Danyaal Rangwala
  */
 
+ 
+ var hexDigits = new Array
+        ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
+		
 /* Current page being displayed */
 var cur_page = "canvas";
 
 /* Current color being used */
-var cur_color = '#ffffff';
+cur_color = '#ffffff';
 
 /* Refresh rate of the Microsoft Kinect (30 Hz) */
 var fps = 30;
 /* Number of consecutive frames an interaction has occurred */
-var frame_count = 0;
+frame_count = 0;
 /* Total distance between each consecutive point of interaction */
 var total_distance = 0;
 
@@ -27,21 +31,21 @@ var total_distance = 0;
 var cur_x = -1;
 var cur_y = -1;
 
-/* Canvas element */
-var c = document.getElementById("myCanvas");
-var context = c.getContext("2d");
-/* Width and height of the canvas */
-c.width = window.innerWidth;
-c.height = window.innerHeight;
-
 $(document).ready(function() {
+
+	/* Canvas element */
+	c = document.getElementById("myCanvas");
+	context = c.getContext("2d");
+	/* Width and height of the canvas */
+	c.width = window.innerWidth;
+	c.height = window.innerHeight;
     fade_border();
 
     /* Wait for user interaction */
     Authority.request("KinectLowestPointCube", {
     relativeto      : Surface.Name,
-    surface_zoffset : 0.02,             // Offset to begin accepting points (in meters)
-    height          : 0.10,             // Offset to stop accepting points (in meters)
+    surface_zoffset : 0.038,             // Offset to begin accepting points (in meters)
+    height          : 0.04,             // Offset to stop accepting points (in meters)
     callback        : "run",            // Function to pass return data from KinectLowestPointCube
     point_limit     : 50,               // Max points to accept
     sendemptyframes : true,             // Callback even if no inputs
@@ -59,7 +63,7 @@ for(var i = 1; i < coords.length; ++i) {
 
 /** Run main canvas loop */
 function run(pointList) {
-    switch(cur_page):
+    switch(cur_page) {
         case "canvas":
             run_canvas(pointList);
             break;
@@ -69,8 +73,9 @@ function run(pointList) {
         case "main_menu":
             run_mmenu(pointList);
             break;
-        case default:
+        default:
             break;
+	}
 }
 
 /** Draw and fade border on page load */
@@ -120,39 +125,68 @@ function random_color() {
  */
 function click_event(pointList) {
     /* More than 40 points registered together */
-    if (pointList.length > 40) {
-        /* Store last position */
-        var last_x = cur_x;
-        var last_y = cur_y;
+	/* Store last position */
+	var last_x = cur_x;
+	var last_y = cur_y;
 
-        /* Get current position */
-        cur_x = comp_avg(pointList, 0).toPrecision(3);
-        cur_y = comp_avg(pointList, 1).toPrecision(3);
+	/* Get current position */
+	cur_x = comp_avg(pointList, 0).toPrecision(3);
+	cur_y = comp_avg(pointList, 1).toPrecision(3);
 
-        /* Get distance between last and current position */
-        var distance = Math.sqrt(Math.pow((last_x + cur_x), 2) +
-            Math.pow((last_y + cur_y), 2));
+	/* Get distance between last and current position */
+	var distance = Math.sqrt(Math.pow((last_x + cur_x), 2) +
+		Math.pow((last_y + cur_y), 2));
 
-        /* If distance is less than 0.1 (position changed by less than 10%) */
-        if (distance < 0.1) {
-            /* If frame_count is one second of frames and average_distance is
-             * less than 0.1 (average position changed by less than 10%) */
-            if (frame_count == seconds_to_frames(1)
-                && total_distance/frame_count < 0.1) {
-                /* Execute click event at current position */
-                $(document.elementFromPoint(cur_x, cur_y)).click();
-            }
-            /* Increment total_distance and frame_count */
-            total_distance += distance;
-            frame_count++;
-        } else {
-            total_distance = 0;
-            frame_count = 0;
-        }
-    } else {
-        total_distance = 0;
-        frame_count = 0;
-    }
+	if (frame_count >= seconds_to_frames(2)) {
+		pixel_x = cur_x * -1 * c.width;
+		pixel_y = cur_y * c.height;
+		
+		/* Defines what action clicking on colorMenu will do */
+		//change to color menu
+		$('#colorMenu').click(function() {
+			frame_count = 0;
+			switch(cur_page) {
+				case "canvas":
+					$('#myCanvas').hide();
+					$('#mainMenuButton').hide();
+					$('.menu').show();
+					cur_page = "color_menu";
+					break;
+				case "color_menu":
+					$('.menu').hide();
+					$('#myCanvas').show();
+					$('#mainMenuButton').show();
+					cur_page = "canvas";
+					break;
+				case "main_menu":
+					$('#mainMenu').hide();
+					$('#myCanvas').show();
+					$('#mainMenuButton').show();
+					cur_page = "canvas";
+					break;
+				default:
+					break;
+			}
+		});
+		$('#mainMenuButton').click(function() {
+			frame_count = 0;
+			$('#myCanvas').hide();
+			$('#mainMenu').show();
+			$('#mainMenuButton').hide();
+			cur_page = "main_menu";
+		});
+		
+		$('.color').click(function() {
+			var rgb = $(this).css('background');
+			cur_color = $(this).attr('value');
+			$('#colorMenu').css('background', rgb);
+			frame_count = 0;
+		});
+		
+		
+		$(document.elementFromPoint(pixel_x, pixel_y)).click();
+	}
+	$('#count').text(frame_count + '');
 }
 
 /**
@@ -170,32 +204,36 @@ function run_canvas(pointList) {
 
             pixel_x = cur_x * -1 * c.width;
             pixel_y = cur_y * c.height;
+			
+			click_event(pointList);
 
             context.beginPath();
-            context.arc(pixel_x, pixel_y, 30, 0, 2*Math.PI);
+            context.arc(pixel_x, pixel_y, 15, 0, 2*Math.PI);
             context.closePath();
-            context.strokeStyle = '#ffffff';
+            context.strokeStyle = '#444444';
             context.fillStyle = cur_color;
             context.fill();
+			
+			frame_count++;
     }
+	else {
+		frame_count = 0;
+	}
 }
 
-/**
- * Block to execute color menu functionality of canvas
- *
- * @param {number} pointList The set of interaction points returned by the
- * Kinect
- */
 function run_cmenu(pointList) {
-    // TODO
-}
+    /* More than 40 points registered together */
+    if (pointList.length > 40) {
+            cur_x = comp_avg(pointList,0).toPrecision(3);
+            cur_y = comp_avg(pointList,1).toPrecision(3);
+            cur_z = comp_avg(pointList,2).toPrecision(3);
 
-/**
- * Block to execute main menu functionality of canvas
- *
- * @param {number} pointList The set of interaction points returned by the
- * Kinect
- */
-function run_mmenu(pointList) {
-    // TODO
+            // pixel_x = cur_x * -1 * c.width;
+            // pixel_y = cur_y * c.height;
+			
+			click_event(pointList);
+			frame_count ++;
+	}	else {
+		frame_count = 0;
+	}
 }
