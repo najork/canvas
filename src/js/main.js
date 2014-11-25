10a35edc5e4c6ba2da12d663b4c3f9782842f7c5
mnajork <mnajork@umich.edu>
2014-11-24 20:55:24 -0500

    Merged canvas.js into main.js

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,9 @@
 /* Current page being displayed */
 var cur_page = "canvas";
 
+/* Current color being used */
+var cur_color = '#ffffff';
+
 /* Refresh rate of the Microsoft Kinect (30 Hz) */
 var fps = 30;
 /* Number of consecutive frames an interaction has occurred */
@@ -21,8 +24,15 @@ var frame_count = 0;
 var total_distance = 0;
 
 /* Average position of the current interaction */
-var cur_x = 0;
-var cur_y = 0;
+var cur_x = -1;
+var cur_y = -1;
+
+/* Canvas element */
+var c = document.getElementById("myCanvas");
+var context = c.getContext("2d");
+/* Width and height of the canvas */
+c.width = window.innerWidth;
+c.height = window.innerHeight;
 
 $(document).ready(function() {
     fade_border();
@@ -39,6 +49,14 @@ $(document).ready(function() {
 
 });
 
+/* Draw on the canvas */
+for(var i = 1; i < coords.length; ++i) {
+    context = c.getContext("2d");
+    context.moveTo(coords[i-1].x, coords[i-1].y);
+    context.lineTo(coords[i].x,coords[i].y);
+    context.stroke();
+}
+
 /** Run main canvas loop */
 function run(pointList) {
     switch(cur_page):
@@ -46,10 +64,10 @@ function run(pointList) {
             run_canvas(pointList);
             break;
         case "color_menu":
-            run_Cmenu(pointList);
+            run_cmenu(pointList);
             break;
         case "main_menu":
-            run_Mmenu(pointList);
+            run_mmenu(pointList);
             break;
         case default:
             break;
@@ -74,9 +92,24 @@ function comp_avg(values, comp) {
  * Kinect (30 Hz)
  *
  * @param {number} seconds The number of seconds to convert to number of frames
+ * @return {number} The number of frames equivalent to the seconds inputted
  */
 function seconds_to_frames(seconds) {
     return seconds * fps;
+}
+
+/**
+ * Generate a random color
+ *
+ * @return {string} A hexadecimal representation of a color
+ */
+function random_color() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 /**
@@ -120,4 +153,49 @@ function click_event(pointList) {
         total_distance = 0;
         frame_count = 0;
     }
+}
+
+/**
+ * Block to execute main draw functionality of canvas
+ *
+ * @param {number} pointList The set of interaction points returned by the
+ * Kinect
+ */
+function run_canvas(pointList) {
+    /* More than 40 points registered together */
+    if (pointList.length > 40) {
+            cur_x = comp_avg(pointList,0).toPrecision(3);
+            cur_y = comp_avg(pointList,1).toPrecision(3);
+            cur_z = comp_avg(pointList,2).toPrecision(3);
+
+            pixel_x = cur_x * -1 * c.width;
+            pixel_y = cur_y * c.height;
+
+            context.beginPath();
+            context.arc(pixel_x, pixel_y, 30, 0, 2*Math.PI);
+            context.closePath();
+            context.strokeStyle = '#ffffff';
+            context.fillStyle = cur_color;
+            context.fill();
+    }
+}
+
+/**
+ * Block to execute color menu functionality of canvas
+ *
+ * @param {number} pointList The set of interaction points returned by the
+ * Kinect
+ */
+function run_cmenu(pointList) {
+    // TODO
+}
+
+/**
+ * Block to execute main menu functionality of canvas
+ *
+ * @param {number} pointList The set of interaction points returned by the
+ * Kinect
+ */
+function run_mmenu(pointList) {
+    // TODO
 }
