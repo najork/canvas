41a4a46a37e7fd6f81b1c743fccb3f83be9958b2
mnajork <mnajork@umich.edu>
2014-11-24 19:48:57 -0500

    Update main.js to have click event

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,81 @@
-cur_page = "canvas";
+/**
+ * ~ Canvas ~
+ *
+ * Description: An interactive coloring "book" web application that utilizes
+ * data from a Microsoft Kinect to enable users to draw on a projected surface
+ * with their body
+ *
+ * Run using Ubi Displays software (found at https://code.google.com/p/ubidisplays)
+ *
+ * Authors: Dustin Bui, Maximilian Najork, Zachary Nowicki, Danyaal Rangwala
+ *
+ */
 
-$(document).ready(function()
-{
-	// Show the border.
-	Surface_PropertiesChanged();
-	
-	// Request the lowest point cube.
-	Authority.request("KinectLowestPointCube", {
-	relativeto : Surface.Name,                      // The surface we want information relative too.
-	surface_zoffset : 0.02, // 0.015        // The height from the surface we want to accept points from (in meters).
-	height:0.10,                                            // The height from the surface+offset we want to no longer accept points from (in meters).
-	callback : "run",       // The function we want to call back with information.
-	point_limit : 50,                                       // The max number of points to accept.
-	sendemptyframes : false,                        // Do we want callbacks when we have empty frames (i.e. when we have no points at all).
-	});
+// The current page being displayed
+var cur_page = "canvas";
+// The number of consecutive frames an interaction has occurred
+var frame_count = 0;
+
+$(document).ready(function() {
+    Fade_Border();
+
+    // Wait for user interaction
+    Authority.request("KinectLowestPointCube", {
+    relativeto      : Surface.Name,
+    surface_zoffset : 0.02,             // Offset to begin accepting points (in meters)
+    height          : 0.10,             // Offset to stop accepting points (in meters)
+    callback        : "run",            // Function to pass return data from KinectLowestPointCube
+    point_limit     : 50,               // Max points to accept
+    sendemptyframes : true,             // Callback even if no inputs
+    });
 
 });
 
-// Little snippit which fades out the border when we are changing the surface properties.
-//   This makes it a bit easier to see when we are dragging the corners.
-function Surface_PropertiesChanged()
-{
-	$("#border").show().delay(2000).fadeOut(500);
+// Draw and fade border on page load
+function Fade_Border() {
+    $("#border").show().delay(2000).fadeOut(500);
 }
-                        
-// The number of frames where the hand has been in place.
-var iFrameCount = 0;
 
-// The starting number of "likes".
-var iLikeCount = 23;
+function run(pointList) {
+    switch(cur_page):
+        case "canvas":
+            run_canvas(pointList);
+            break;
+        case "color_menu":
+            run_Cmenu(pointList);
+            break;
+        case "main_menu":
+            run_Mmenu(pointList);
+            break;
+        case default:
+            break;
+}
 
-function run(pointList){
-	switch(cur_page):
-		case "canvas":
-			run_canvas(pointList);
-			break;
-		case "color_menu":
-			run_Cmenu(pointList);
-			break;
-		case "main_menu":
-			run_Mmenu(pointList);
-			break;
-		case default:
-			break;
+function comp_avg(values, comp) {
+    var cnt, tot, i;
+    cnt = values.length;
+    tot = i = 0;
+    while (i < cnt) tot+= values[i++][comp];
+    return tot / cnt;
+}
+
+function clickEvent(pointList) {
+    // If more than 40 points registered together
+    if (pointList.length > 40) {
+        var last_x =
+        avg_pos_x = comp_avg(pointList,0).toPrecision(3);
+        avg_pos_y = comp_avg(pointList,1).toPrecision(3);
+
+        // If points have been in frame for 1 second
+        if (frame_count == 30) {
+            avg_pos_x = comp_avg(pointList,0).toPrecision(3);
+            avg_pos_y = comp_avg(pointList,1).toPrecision(3);
+
+            $(document.elementFromPoint(avg_pos_x, avg_pos_y)).click();
+        }
+        frame_count++;
+    } else {
+        frame_count = 0;
+    }
 }
 
